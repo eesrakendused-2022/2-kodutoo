443beb0c2d6b6a65d9c755d5f452257e9c26d7db
Kristjan Tamm <90192493+TammKristjan@users.noreply.github.com>
2022-05-08 20:34:35 +0300

    Game working

--- pingpong.js
+++ pingpong.js
@@ -1,68 +1,28 @@
-/* 
+
 class Pingpong{
     constructor(){
-        this.speed = 15;
-        this.r = document.getElementById('right');
-        this.l = document.getElementById('left');
-        this.b = document.getElementById('ball');
+
+        this.speed = 30;
+        
+        this.right = document.getElementById('right');
+        this.left = document.getElementById('left');
+        this.ball = document.getElementById('ball');
 
         this.rscore = document.getElementById('scoreleft');
         this.lscore = document.getElementById('scoreright');
         this.ogoal = document.getElementById('goal');
 
-        this.w = window.innerWidth;
-        this.h = window.innerHeight;
+        this.width = window.innerWidth;
+        this.height = window.innerHeight;
 
-        this.key = []; // Or you could call it "key"
-    }
-
-    nfp(urpx) {
-        return Number(urpx.replace("px", ""))
-    }
-
-    onkeydown = onkeyup = function(e) {
-        e = e || event; // to deal with IE
-        this.key[e.keyCode] = e.type == 'keydown';
-    }
-    
-
-    keydown() {
-       //if key was up arrow
-        if (this.key[40]) {
-            if (nfp(this.r.style.top) + this.speed > this.h - 200){
-                this.r.style.top = this.h - 200 + "px";
-            }
-                
-            else{
-                this.r.style.top = nfp(this.r.style.top) + this.speed + "px";
-            }
-        }
-
-
-
-        //if key was down arrow
-        else if (key[38]) {
-            if (nfp(r.style.top) - ps < 0){
-                r.style.top = 0 + "px";
-            }
-            else{
-                r.style.top = nfp(r.style.top) - ps + "px";
-            }
-        }
-    
-        //40 down, 38 up
-        //w 87,s 83
-    }
-}
-
-let pingpong = new Pingpong();
-
- */
-
-class Pingpong{
-    constructor(){
+        this.speedx = 6;
+        this.speedy = 1;
+        this.balltime = 1;
+        this.ball.style.left = this.width / 2 + "px";
         
+        $('#goal').hide();
         this.keypress();
+        this.moveBall();
     }
 
     nfp(urpx){
@@ -70,61 +30,102 @@ class Pingpong{
     }
 
     keypress(){
-        $(document).on('keypress',function(event) {
-            if(event.which == 13) {
-                this.test();
+        
+        $(document).on('keydown',(event)=> {
+            // down arror
+            if(event.which == 40) {
+                if (this.nfp(this.right.style.top) + this.speed > this.height - 200){
+                    this.right.style.top = this.height - 200 + "px";
+                }
+                else{
+                    this.right.style.top = this.nfp(this.right.style.top) + this.speed + "px";
+                }
+                // up arrow
+            } else if (event.which == 38){
+                if (this.nfp(this.right.style.top) - this.speed < 0){
+                    this.right.style.top = 0 + "px";
+                }
+                else{
+                    this.right.style.top = this.nfp(this.right.style.top) - this.speed + "px";
+                }
+            }
+
+            // s
+            if(event.which == 83) {
+                if (this.nfp(this.left.style.top) + this.speed > this.height - 200){
+                    this.left.style.top = this.height - 200 + "px";
+                }
+                else{
+                    this.left.style.top = this.nfp(this.left.style.top) + this.speed + "px";
+                }
+                // w
+            } else if (event.which == 87){
+                if (this.nfp(this.left.style.top) - this.speed < 0){
+                    this.left.style.top = 0 + "px";
+                }
+                else{
+                    this.left.style.top = this.nfp(this.left.style.top) - this.speed + "px";
+                }
             }
         });
     }
 
-    
+    setBall() {
+        this.ball.style.left = this.nfp(this.ball.style.left) + this.speedx + "px";
+        this.ball.style.top = this.nfp(this.ball.style.top) + this.speedy + "px";
+    }
 
+    moveBall() {
+        this.setBall();
     
-    test(){
-        var speed = 15;
-        
-        var r = document.getElementById('right');
-        console.log("pressed enter")
-        if (this.nfp(r.style.top) - speed < 0){
-            r.style.top = 0 + "px";
-            console.log("1");
+        //remove overflow y
+        if (this.height < this.nfp(this.ball.style.top) + 20 || this.nfp(this.ball.style.top) < 0) {
+            this.speedy *= -1;
+        }
+    
+        //overflow-x right
+        if (this.nfp(this.ball.style.left) >= this.width - 50) {
+            if (this.nfp(this.right.style.top) <= this.nfp(this.ball.style.top) + 20 && this.nfp(this.right.style.top) + 200 >= this.nfp(this.ball.style.top)) {
+                this.speedx *= -1;
+            } else if (this.nfp(this.ball.style.left) >= this.width - 20)
+                this.goal('left');
+        }
+    
+        //remove overflow x in left or get the goal in left
+        if (this.nfp(this.ball.style.left) <= 30) {
+            if (this.nfp(this.left.style.top) <= this.nfp(this.ball.style.top) + 20 && this.nfp(this.left.style.top) + 200 >= this.nfp(this.ball.style.top)) {
+                this.speedx *= -1;
+            } else if (this.nfp(this.ball.style.left) <= 0)
+                this.goal('right');
+        }
+    
+        setTimeout(() => {
+            this.moveBall();
+        }, this.balltime);
+    }
+
+    goal(pos) {
+
+        $('#goal').show();
+    
+        setTimeout(()=> {
+            $('#goal').hide();
+        }, 1000);
+    
+        if (pos == "left"){
+            this.rscore.innerHTML = Number(this.rscore.innerHTML) + 1;
         }
         else{
-            r.style.top = this.nfp(r.style.top) - speed + "px";
-            console.log("2");
+            this.lscore.innerHTML = Number(this.lscore.innerHTML) + 1;
         }
+    
+        this.speedx *= -1;
+        this.ball.style.left = this.width / 2 + "px";
+    
     }
-    
-    
+}
 
-    
-    
 
-    kirjuta(keypressed){
-        console.log(keypressed)
-        var speed = 15;
-        var r = document.getElementById('right');
-        this.l = document.getElementById('left');
-        this.b = document.getElementById('ball');
-
-        this.rscore = document.getElementById('scoreleft');
-        this.lscore = document.getElementById('scoreright');
-        this.ogoal = document.getElementById('goal');
-
-        this.w = window.innerWidth;
-        this.h = window.innerHeight;
-
-        if (keypressed == 's') {
-            
-            if (this.nfp(r.style.top) - speed < 0){
-                r.style.top = 0 + "px";
-            }
-            else{
-                r.style.top = this.nfp(r.style.top) - speed + "px";
-            }
-        }
-        }
-    }
 
 let pingpong = new Pingpong();
 
