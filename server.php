c15aa4896b7728fb05d4219ec9b2385dca929ee4
Ervin Puust <90192519+Ervpst@users.noreply.github.com>
2022-05-10 23:17:36 +0100

    Update server.php

--- server.php
+++ server.php
@@ -1,12 +1,4 @@
 <?php
-if(isset($_POST["save"])&&!empty($_POST["save"])){
-    saveToFile($_POST["save"]);
-}
-
-function saveToFile($stringToSave){
-    $object=new StdClass();
-    $object->last_modified=time();
-    $object->content=$stringToSave;
-    $jsonString=json_encode($object);
-    file_put_contents("database.txt",$jsonString);
-}
+$result = [];
+$result['responsed'] = $_POST['name'];
+echo json_encode($result);
