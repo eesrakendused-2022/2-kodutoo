90bc66b54502ab2125731cac18dbda9ea0dad9ff
Marleene Ristjan <90316863+MarleeneR@users.noreply.github.com>
2022-05-17 15:52:07 +0300

    Add files via upload

--- calculate.js
+++ calculate.js
@@ -3,221 +3,192 @@
         this.name;
         this.startTime = 0;
         this.endTime = 0;
-        this.operationsCompleted = 0;
         this.results = [];
         this.right = 0;
 
         this.timesAnswered = 0;
+        this.number1 = 0;
+        this.number2 = 0;
 
-        //this.loadFromFile();
-       
+        this.easy = 0;
+        this.hard = 0;
+        this.difficultyLevel = "";
+        
+        this.answerBox = document.getElementById('answerId');
+        this.answerBox.addEventListener('keydown', (e) => {
+            if (e.key === 'Enter') {
+                this.checkAnswer(e);
+            }
+        });
+        this.exercise = document.getElementById('exercise');
 
-        this.startGame();
+        $('#results').hide();
+        this.loadFromFile();
     }
 
-    /*loadFromFile(){
+    
+
+    loadFromFile(){
         $.get("database.txt", (data) => {
             let content = JSON.parse(data).content;
             console.log(content);
             this.results = content;
             localStorage.setItem('score', JSON.stringify(content));
         }); 
-    }*/
+        
+        this.checkIfNameWriten();
+        
+    }
 
-    /* checkIfNameWriten(){
+    checkIfNameWriten(){
+        this.easy = 0;
+        this.hard = 0;
     $('#submitName').click(()=>{
         this.name= $('#nameValue').val(); 
+
+        if($('#difficultyLevelEasy').is(':checked')) {
+            this.easy = 1; 
+            }
+        if($('#difficultyLevelHard').is(':checked')) {
+            this.hard = 1;  
+            }
+    
             this.startGame(); 
             $('#name').hide();
 
             console.log(this.name);
-        })
-    } */
+            console.log(this.easy);
+            console.log(this.hard);
 
-    
+           
+        })
+    }
+
+
+
 
     startGame(){
         this.generateNewOperation();
         this.startTime = performance.now();
-        //$(document).off('keypress');
-        //$(document).on('keypress', (event)=>this.saveEndTime(event.key));
-        //this.updateGameInfo();  
-        //this.showResults();   T
     }
 
 
    generateNewOperation() {
         exercises = document.getElementById("exercises");
-        exercises.innerHTML = "";
-        
-
-            //--------------------------
-            // teeb 5 tehet
-        //for(var i = 0; i < 1; i++){
-            var exercise = document.createElement("div");
-            var answerBox = document.createElement("input");
-            answerBox.id = 'answerId';
             
-            //var exercise_type = Math.floor(Math.random() * 4);
-            var number1;
-            var number2;
-            
-            //if(exercise_type == 0){
-                number1 = Math.floor(Math.random() * 20) + 1;
-                number2 = Math.floor(Math.random() * 20) + 1;
-                answerBox.solution = number1 + number2;
-                exercise.innerHTML = number1 + " + " + number2 + " = ";
-            //}
-
-            exercise.append(answerBox);
-            
-            exercises.append(exercise);
-        //}
-
-
-        answerBox = document.getElementById('answerId');
-         if(answerBox.value == answerBox.solution){
-                this.right++;
+            if(this.easy == 1){
+                this.number1 = Math.floor(Math.random() * 20) + 1;
+                this.number2 = Math.floor(Math.random() * 20) + 1;
+                this.answerBox.solution = this.number1 + this.number2;
+                this.exercise.innerHTML = this.number1 + " + " + this.number2 + " = ";
+                this.difficultyLevel = "Lihtne";
             }
-            console.log(this.right);
-            console.log(answerBox.value);
-            console.log(answerBox.solution); 
 
+            if(this.hard == 1){
+                this.number1 = Math.floor(Math.random() * 20) + 1;
+                this.number2 = Math.floor(Math.random() * 20) + 1;
+                this.answerBox.solution = this.number1 * this.number2;
+                this.exercise.innerHTML = this.number1 + " x " + this.number2 + " = ";
+                this.difficultyLevel = "Raske";
+            }
 
-
-            answerBox.addEventListener('keydown', (e) => {
-                if (e.key === 'Enter') {
-                    //this.generateNewOperation(e);
-                    //this.checkAnswer(e);
-
-
-                    this.timesAnswered++;
-
-                    if(this.timesAnswered <= 3){
-                        this.generateNewOperation(e);
-                    }
-                }
-            });
-
-            //this.checkAnswer();
+           
+            exercises.append(this.answerBox);
 
             console.log(this.startTime);
     }
 
 
-    
 
+    checkAnswer(){
 
-
-    /*checkAnswer(){
-        //answ = document.getElementById('exercises').value;
-        //console.log(answ);
-
-        for(var i = 0; i < 1; i++){
-        answerBox = document.getElementById('answerId');
-        if(answerBox.value == answerBox.solution){
+        if(this.answerBox.value == this.answerBox.solution){
             this.right++;
         }
         console.log(this.right);
-        console.log(answerBox.value);
-        console.log(answerBox.solution);
-    }
+        console.log(this.answerBox.value);
+        console.log(this.answerBox.solution);
+    
 
+        this.answerBox.value = "";
+        this.exercise.innerHTML = "";
+
+    this.timesAnswered++;
+    console.log(this.timesAnswered);
+
+    if(this.timesAnswered >= 4){
+        this.saveEndTime();
+       
+    }else{
         this.generateNewOperation();
+    }   
     }
-*/
+
         
 
 
+    saveEndTime(){
+        this.endTime = performance.now();
+        $('#results').show();
+        $('#exercises').hide();
+        $('#startNew').show();
+        this.saveResults();
 
-
-
-    //VASTUSTE KONTROLL
-
-
-    /*check_exercises = function(){
-        var generate = document.getElementById("generate");
-        //generate.innerHTML = "Generate Exercises";
-        //generate.onclick= generate_exercises;
-        
-        var right = 0;
-        
-        for(var i = 0; i < 5; i++)
-        {
-            answerBox = document.getElementById(i);
-            if(answerBox.value == answerBox.solution)
-            {
-                right++;
-                //experience++;
-            }
-        }
-        
-        exercises = document.getElementById("exercises");
-        exercises.innerHTML = "You have solved " + right + " of 10 exercises successfully!";
-        //increase_level();
-        show_stats();
-
-    } */
-    
-    
-
-    //----------------------------------------
-
-
-   
-
-    saveEndTime(keypressed){
-    console.log(keypressed);
-    this.endTime = performance.now();
-    $('#results').html(this.name + " sinu aeg oli " + ((this.endTime-this.startTime)/1000).toFixed(2));
-    $('#exercises').hide();
-    $('#startNew').show();
-    this.saveResults();
-    $(document).off('keypress');
-    $(document).on('keypress', (event) =>{
-        this.startNewGame(event.keyCode);
-    })
+        document.getElementById("startNewBtn").addEventListener("click", this.startNewGame);
     } 
 
 
-    startNewGame(key){
-    console.log(key)
-
-    if(key===114){
-        
-        this.startGame();
-        $('#exercises').show();
-        $('#startNew').hide();
-    }
-    }
-
-    saveResults(){
-    let result = {
-        name: this.name,
-        time: ((this.endTime-this.startTime)/1000).toFixed(2),
-    }
-
-
-
-    this.results.push(result);
-
-    this.sortResults();
-
-    this.results.splice(10, 10);
-
-    localStorage.setItem('score', JSON.stringify(this.results));
-
-    $.post('server.php', {save: this.results}).done(function(){
-        console.log('Success');
-    }).fail(function(){
-        alert('FAIL');
-    }).always(
-        function(){
-            console.log('Tegime midagi AJAXiga');
+        startNewGame(){
+            $('#name').show();
+            $('#startNew').hide();
+            $('#results').hide();
+            $('#exercises').show();
+            //this.generateNewOperation();
+            this.startTime = performance.now();
+            //-----//this.checkIfNameWriten();
+            this.loadFromFile();
         }
-    )
+    
 
-    this.showResults();
-    }
+        saveResults(){
+        let result = {
+            name: this.name,
+            time: ((this.endTime-this.startTime)/1000).toFixed(2),
+            rightanswers: this.right,
+            difficulty: this.difficultyLevel,
+        }
+        this.timesAnswered = 0;
+        this.easy = 0;
+        this.hard = 0;
+        this.right = 0;
+
+        this.results.push(result);
+        this.results.splice(10, 10);
+
+        localStorage.setItem('score', JSON.stringify(this.results));
+
+        $.post('server.php', {save: this.results}).done(function(){
+            console.log('Success');
+        }).fail(function(){
+            alert('FAIL');
+        }).always(
+            function(){
+                console.log('Tegime midagi AJAXiga');
+            }
+        )
+
+        this.showResults();
+        } 
+
+
+        showResults(){
+            $('#results').html("");
+            for(let i = 0; i < this.results.length; i++){
+                if(i === 10){break;}
+                $('#results').append('<div class="'+ i +'"><div>' + (i+1) + '. ' + this.results[i].name + '</div><div> ' + this.results[i].time + '</div><div> ' + 'õigeid vastuseid: ' + this.results[i].rightanswers + '</div><div>' + 'raskusaste: ' + this.results[i].difficulty + '</div></div>');
+                $("." + i).click(()=>{this.deleteResult(i)});
+            }
+        }
+   
 }
 let mathgame = new MathGame();
